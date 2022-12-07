a70c3746db46b593a8dbf5b11cc687a65ef26b4f
Ronny Ahero <38691932+ron-don@users.noreply.github.com>
2022-12-07 11:27:26 +0300

    HTTP Basic Authentication Config

--- SpringBootSecurity/UnsecureExample/src/main/java/rc/bootsecurity/BootSecurityApplication.java
+++ SpringBootSecurity/UnsecureExample/src/main/java/rc/bootsecurity/BootSecurityApplication.java
@@ -1,7 +1,9 @@
 package rc.bootsecurity;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class BootSecurityApplication {
 
     public static void main(String[] args) {
